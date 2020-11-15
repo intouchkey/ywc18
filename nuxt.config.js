99aa29233cb548911cad873f9324d06546637a2f
Intouch Yutinatorn <keyeducation2544@gmail.com>
2020-11-15 20:30:13 +0700

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  router: 'ywc18',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'ywc18',

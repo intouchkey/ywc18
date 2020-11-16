f244379a85f11d2a076a427b98d802f0e8554e1f
Intouch Yutinatorn <keyeducation2544@gmail.com>
2020-11-16 14:38:30 +0700

    header fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <template>
 <div>
   <b-nav tabs align="center" class="header">
-    <b-navbar-brand href="#">
-      <img src="https://search-merchant.คนละครึ่ง.com/images/halfhalf-logo.png" class="d-inline-block align-top">
-      <span class="input-group md-form form-sm form-2 pl-0">
-        <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
-        <span class="input-group-append">
-          <span class="input-group-text red lighten-3" id="basic-text1"><i class="fas fa-search text-grey"
+    <b-navbar-brand href="#" class= "col-12 d-flex">
+      <div class="row col-12 align-items-center">
+          <div class="col-md-3 col-sm-2">
+          <img class="d-none d-sm-block" src="https://search-merchant.คนละครึ่ง.com/images/halfhalf-logo.png">
+          <img class="d-sm-none" src="https://search-merchant.คนละครึ่ง.com/images/halfhalf-logo-mini.png">
+         </div>
+        <div class="col-md-9 col-sm-10 search-container">
+            <div class="col-3 d-none d-sm-block"> </div>
+             <input class="col-md-7  form-control my-0 py-1 red-border" type="text" placeholder="ค้นหา ชื่อ ร้านอาหาร และเครื่องดื่ม ร้านธงฟ้า ร้านค้า OTOP และสินค้าทั่วไป" aria-label="Search">
+             <span class="input-group-text red lighten-3 col-2" id="basic-text1"><i class="fas fa-search text-grey"
+                aria-hidden="true"></i></span>
+          
+        </div>
+      </div>
+      <!--<img src="https://search-merchant.คนละครึ่ง.com/images/halfhalf-logo.png" class="col-4 d-inline-block align-top">
+      <span class="col-8 justify-content-center align-items-center input-group md-form form-sm form-2 pl-0">
+        <input class="form-control my-0 py-1 red-border" type="text" placeholder="ค้นหา ชื่อ ร้านอาหาร และเครื่องดื่ม ร้านธงฟ้า ร้านค้า OTOP และสินค้าทั่วไป" aria-label="Search">
+        <span class="input-group-append search-button">
+          <span class="input-group-text red lighten-3 " id="basic-text1"><i class="fas fa-search text-grey"
               aria-hidden="true"></i></span>
         </span>
-      </span>
+      </span>-->
     </b-navbar-brand>
   </b-nav>
   <b-nav class="custom-bg-dark-blue">
@@ -25,6 +38,10 @@ export default {}
 </script>
 
 <style>
+.row{
+  padding:0;
+  margin:0;
+}
 .custom-bg-dark-blue {
   background-color: #293A7C; 
 } 
@@ -39,5 +56,8 @@ p {
 .header: {
   display: inline;
   
+}
+.grey-search-bar{
+  border-radius:6%; 
 }
 </style>
